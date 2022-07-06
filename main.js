106e452924eb5a6b420f87a7f5558dcdac3db070
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-07-06 16:59:34 +0700

    2.6 not finished

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            text: 'something',
             product: 'Shoes',
             image: './assets/images/socks_green.jpg',
-            inStock: true,
+            inStock: false,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             cart: 0
 
@@ -20,6 +21,14 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        changeText(){
+            if(inStock==true){
+                text = 'Add to Cart'
+            }
+            else
+            text ='Out of Stock',
+            image ='./assets/images/OutOfStock.jpg'
         }
     }
 })
