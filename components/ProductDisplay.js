4f89d8afc81d02be7775b669775d92a7297b06a8
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-07-07 18:38:31 +0700

    5.5 -5.6 stage not finished

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -21,13 +21,11 @@ app.component('product-display',{
 				<p v-else> Out of Stock </p>
 				<p >Shipping:{{shipping}} </p>
 
-				<div
-					v-for="(variant, index) in variants"
-					:key="variant.id"
-					@mouseover="updateVariant(index)"
-					class="color-circle"
-					:style="{ backgroundColor: variant.color}">
-				</div>
+				<div v-for="(variant,index) in variants" 
+				:key="variant.id"
+				@mouseover="updateVariant(index)" 
+				class="color-circle" 
+				:style="{backgroundColor: variant.color}"></div>
 				
 				<button
 					class="button"
@@ -35,6 +33,13 @@ app.component('product-display',{
 					:disabled="!inStock"
 					v-on:click="addToCart">
 					Add to Cart
+				</button>
+				<button 
+				class="button"
+				v-on:click="cleanCart">
+				Empty Cart
+					
+
 				</button>
 
 				<product-detail :details="details"></product-detail>
@@ -54,27 +59,31 @@ app.component('product-display',{
 		    product: 'Shoes',
 		    brand:'SE 331',
 		    onsale:true,
-		    image: './assets/images/socks_green.jpg',
-		    inStock: true,
 		    inventory: 100,
 		    details: ['50% cotton', '30% wool', '20% polyester'],
 		    variants: [
-			{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
-			{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' ,quantity: 0},
+			{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50,total:0},
+			{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' ,quantity: 50,total:0},
 		    ],
+		    selectedVariant: 0 ,
 		    cart: 0
 	
 		}
 	    },
 	    methods: {
 		addToCart() {
-		    this.cart += 1
+		    this.$emit('add-to-cart',this.variants[this.selectedVariant].id)
+		},
+		cleanCart(){
+			this.$emit(this.variants.id = [])
 		},
 		updateImage(variantImage) {
 		    this.image = variantImage
+		    
 		},
 		updateVariant(index){
 		    this.selectedVariant = index;
+		    console.log('i like balls');
 		}
 	    },
 	
--- main.js
+++ main.js
@@ -5,36 +5,29 @@ const app = Vue.createApp({
             product: 'Shoes',
             brand:'SE 331',
             onsale:true,
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' ,quantity: 0},
             ],
-            cart: 0,
+            cart: [],
             premium: true
 
         }
     },
 
     methods: {
+        updateCart(id){
+            this.cart.push(id);
+        },
         addToCart() {
             this.cart += 1
         },
         updateImage(variantImage) {
             this.image = variantImage
         },
-        setPic(){
-            if(inStock==true){
-                text = 'Add to Cart'
-            }
-            else
-            text ='Out of Stock',
-            image ='./assets/images/OutOfStock.jpg'
-        },
-        upateVariant(index){
+        updateVariant(index){
             this.selectedVariant = index;
         }
     },
